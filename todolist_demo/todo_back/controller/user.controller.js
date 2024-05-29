4cba4e79cfc4bcf77b03382783ccf2fd3593a0c8
Seam Park <run128@naver.com>
2024-05-30 01:38:40 +0900

    await add

--- todolist_demo/todo_back/controller/user.controller.js
+++ todolist_demo/todo_back/controller/user.controller.js
@@ -47,7 +47,7 @@ userController.loginWithEmail = async (req, res) => {
 userController.getUser = async (req, res) => {
   try {
     const { userId } = req;
-    const user = User.findById(userId);
+    const user = await User.findById(userId);
 
     if (!user) {
       throw new Error("can not find user");
